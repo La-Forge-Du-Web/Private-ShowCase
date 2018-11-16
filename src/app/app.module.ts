ed2a2c0c10c9b542840e7548bbd9e66e2c6bab4e
Malhju <laforgeduweb@gmail.com>
2018-11-16 23:37:55 +0100

    US01 without colors, texts, titles or images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+
+import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeToolbarComponent } from './welcome-toolbar/welcome-toolbar.component';
+import { ExampleCardComponent } from './example-card/example-card.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    WelcomeToolbarComponent,
+    ExampleCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
